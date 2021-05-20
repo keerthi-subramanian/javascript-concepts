d5f91d26853cf3fe258aa93c1a7906cd3937f5dc
Keerthi Shankar Subramanian <keerthi.subramanian@mitigram.com>
2021-05-20 14:52:06 +0200

    Add functions example code snippet

---
+++ 1.Functions/Functions.js
@@ -0,0 +1,30 @@
+// Normal function:
+function add(a, b) {
+    return a + b
+}
+
+const c = add(1, 2)
+console.log(`Value of c is ${c}`)
+
+
+//------------------------------
+
+
+// Arrow function:
+addArrowFunction = (i, j) => {
+    return i + j
+}
+
+const d = addArrowFunction(2, 3)
+console.log(`Value of d is ${d}`)
+
+
+//------------------------------
+
+// Immediately invoked function
+;(
+    function immediatelyInvokedFunction() {
+        const addTwoNumbers = 10 + 20
+        console.log(`Value of z is ${addTwoNumbers}`)
+    }
+)()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# javascript-concepts
+# Javascript Concepts
+Look into individual directories for code samples
+
+### Executing a Javascript file(a.k.a module)
+1. Open a terminal
+2. Use `cd` command to cd to the path of the JS file
+3. Use `node <file_name>` to execute the JS file
